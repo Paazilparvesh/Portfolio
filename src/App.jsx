138a84949cc8b6ea1acd8a17a776374438922c47
Thirdvizion-labs <git-3rdviz@thirdvizion.com>
2025-07-11 13:54:54 +0530

    splitted project and about page

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ function App() {
     
     <BrowserRouter>
     <Pointer />
-    <ScrollToTop /> {/* Always keep this above <Routes /> */}
     <Header />
+    <ScrollToTop /> {/* Always keep this above <Routes /> */}
       <Routes>
         <Route path="/" element={<ProjectsPage />} />
         <Route path="/about" element={<About />} />
