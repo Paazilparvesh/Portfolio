3ea38b977621e78a74f9f038052384602ebfdeda
Thirdvizion-labs <git-3rdviz@thirdvizion.com>
2025-07-24 16:26:18 +0530

    changed gifs all

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section className="relative flex justify-center items-center bg-[#FFB91A] group w-full h-screen overflow-hidden">
       {/* Background & Content */}
       <div className="relative w-full h-full flex items-center md:mt-5 bg-cover bg-center bg-no-repeat px-6 sm:px-10 md:px-20 pb-[140px] md:pt-[140px]">
-        <div className="relative flex flex-col items-start justify-center md:h-screen z-10 max-w-screen-xl h-screen mx-auto w-full lg:w-1/2 text-center">
+        <div className="relative flex flex-col items-start justify-center md:h-screen z-10 max-w-screen-xl h-screen mx-auto w-full lg:w-1/2 text-center -mt-30 md:mt-0">
           {/* 🅰 Hero Title */}
           <p className="w-full text-center md:text-left text-white font-helvetica text-sm sm:text-base md:text-md xl:text-3xl tracking-widest mb-4 lg:mb-6 xl:mb-10">
             Hello, I’m
